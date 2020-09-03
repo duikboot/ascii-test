e6e97e7a4dbaa51bffb0425bd68a024450626219
Arjen Dijksta <dijkstra.arjen@gmail.com>
2020-09-03 11:41:28 +0200

    added autogenerated file

--- ascii_art.py
+++ ascii_art.py
@@ -1,31 +1,32 @@
 import characters
 
 
-def index_character(char):
-    """Return the Unicode code point for a one-character string. """
-    return ord(char)
-
-
 def get_ascii_character(index):
     """Return the ascii character of the index."""
+
     return characters.characters[index]
 
 
 def get_zipped_chars(text):
+    """Return zipped list of ascii characters, so we can iterate over it."""
     characters_list = []
 
-    for i in text:
-        ascii_char = get_ascii_character(index_character(i))
+    for index in text:
+        ascii_char = get_ascii_character(ord(index))
         characters_list.append(ascii_char.splitlines())
 
     return zip(*characters_list)
 
 
-if __name__ == "__main__":
+def main():
     user_text = input("Give text for ascii art: ")
     zipped_chars = get_zipped_chars(user_text)
 
-    for line in zipped_chars:
-        for c in line:
-            print(c, end="")
+    for lines in zipped_chars:
+        for line in lines:
+            print(line, end="")
         print()
+
+
+if __name__ == "__main__":
+    main()
